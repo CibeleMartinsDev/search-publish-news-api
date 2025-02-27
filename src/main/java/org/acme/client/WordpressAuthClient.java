e228f2e8d66ec27d1b978005d1497b09dcd8e889
CibeleMartinsDev <cibelemdev@gmail.com>
2025-02-26 22:24:41 -0300

    FEAT: add WordpressAuthClient and WordpressAuthRequest

---
+++ src/main/java/org/acme/client/WordpressAuthClient.java
@@ -0,0 +1,21 @@
+package org.acme.client;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+
+@Path("/wp-json/jwt-auth/v1/token")
+@RegisterRestClient(baseUri = "${WORDPRESS_SITE_URL}")
+public interface WordpressAuthClient {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    String getAuthorization(
+            @HeaderParam("Authorization") String authorization,  // Header para o Token JWT
+            String postBody  // Corpo da requisição JSON
+    );
+
+
+}
