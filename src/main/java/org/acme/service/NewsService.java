fe56e5219a81efb52f6a9e467981c96657f18ecc
CibeleMartinsDev <cibelemdev@gmail.com>
2025-02-26 22:33:16 -0300

    feat: add comments

--- src/main/java/org/acme/service/NewsService.java
+++ src/main/java/org/acme/service/NewsService.java
@@ -3,11 +3,6 @@ package org.acme.service;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import okhttp3.HttpUrl;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import org.acme.client.NewsApiClient;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
@@ -24,12 +19,27 @@ public class NewsService {
     @ConfigProperty(name = "myapp.thenews.api.key")
     String apiToken;
 
-    @GET
     public String getNews() {
         String search = "Santa Fé,Paraná";
         int limit = 3;
 
         // Fazendo a chamada para a API de notícias
         return newsApiClient.getNews(apiToken,  search,"pt","br", limit);
+    }
+
+    public void publishNews() {
+        //receber o 6 tipos de busca
+
+        //buscar 6 noticias
+
+        //pegar a url de cada uma
+
+        //extrair o conteúdo das 6 noticias
+
+        //criar um post wordpress p/ cada notícia
+
+        //passar o conteúdo da notícia p/ post wordpress
+
+        //publicar
     }
 }
