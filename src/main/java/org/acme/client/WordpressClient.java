17c58866f783c2a57ada3e57b6efe10adbbe88af
CibeleMartinsDev <cibelemdev@gmail.com>
2025-02-26 22:25:16 -0300

    refactor: WordpressClient and remove GreetingResource.java

--- src/main/java/org/acme/client/WordpressClient.java
+++ src/main/java/org/acme/client/WordpressClient.java
@@ -1,4 +1,30 @@
 package org.acme.client;
 
-public class WordpressClient {
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+
+@Path("/wp-json/wp/v2/posts")
+@RegisterRestClient(baseUri = "${WORDPRESS_SITE_URL}")
+public interface WordpressClient {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    String createPost(
+            @HeaderParam("Authorization") String authorization,  // Header para o Token JWT
+            String postBody  // Corpo da requisição JSON
+    );
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    String getPostById(
+            @PathParam("id") int id,  // ID do post a ser recuperado
+            @HeaderParam("Authorization") String authorization   // Passando o token JWT no cabeçalho
+    );
+
 }
+
+
