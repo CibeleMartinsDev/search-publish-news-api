55471fca067221dd0429cd3b0be69154de9160cf
CibeleMartinsDev <cibelemdev@gmail.com>
2025-03-03 17:16:34 -0300

    feat: change NewsService, add try and catch, exceptions, and new parameters in the methods

--- src/main/java/org/acme/service/NewsService.java
+++ src/main/java/org/acme/service/NewsService.java
@@ -1,13 +1,17 @@
 package org.acme.service;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.acme.client.DiffBotClient;
 import org.acme.client.NewsApiClient;
+import org.acme.dto.DiffBotNewsRequest;
+import org.acme.dto.DiffBotNewsResponse;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
-import java.io.IOException;
+import java.util.ArrayList;
 
 
 @ApplicationScoped
@@ -19,27 +23,55 @@ public class NewsService {
     @ConfigProperty(name = "myapp.thenews.api.key")
     String apiToken;
 
-    public String getNews() {
-        String search = "Santa Fé,Paraná";
-        int limit = 3;
+    @Inject
+    @RestClient
+    DiffBotClient diffBotClient;
 
-        // Fazendo a chamada para a API de notícias
-        return newsApiClient.getNews(apiToken,  search,"pt","br", limit);
+    @ConfigProperty(name = "myapp.diffbot.api.key")
+    String configValue;
+
+
+    public String getNews(String search,
+                          String locale,
+                          int limit,
+                          String searchFields,
+                          String categories,
+                          String excludeCategories,
+                          String domains,
+                          String excludeDomains,
+                          String sourceIds,
+                          String excludeSourceIds,
+                          String language,
+                          String publishedBefore,
+                          String publishedAfter,
+                          String publishedOn,
+                          String sort,
+                          Integer page) throws Exception {
+        try {
+            // Fazendo a chamada para a API de notícias
+            return newsApiClient.getNews(apiToken, search, locale, limit,
+                    searchFields, categories, excludeCategories, domains,
+                    excludeDomains, sourceIds, excludeSourceIds, language,
+                    publishedBefore, publishedAfter, publishedOn, sort, page);
+        } catch (Exception e) {
+            throw new Exception("Erro pesquisar as notícias. Tente novamente em instantes.");
+        }
+
     }
 
-    public void publishNews(String search) {
-        //receber o 6 tipos de busca
-
-        //buscar 6 noticias
-
-        //pegar a url de cada uma
-
-        //extrair o conteúdo das 6 noticias
-
-        //criar um post wordpress p/ cada notícia
-
-        //passar o conteúdo da notícia p/ post wordpress
-
-        //publicar
+    public ArrayList<DiffBotNewsResponse.Object> getContentNews(DiffBotNewsRequest diffBotNewsRequest) throws Exception {
+        try {
+            ArrayList<DiffBotNewsResponse.Object> result = new ArrayList<>();
+            for (String url : diffBotNewsRequest.getUrls()) {
+                String response = diffBotClient.getContentNews(configValue, url, "1000");
+                ObjectMapper objectMapper = new ObjectMapper();
+                DiffBotNewsResponse diffBotNewsResponse = objectMapper.readValue(response, DiffBotNewsResponse.class);
+                result.addAll(diffBotNewsResponse.getObjects());
+            }
+            return result;
+        } catch (Exception e) {
+            throw new Exception("Erro ao extrair o conteúdo das notícias. Tente novamente em instantes.");
+        }
     }
+
 }
